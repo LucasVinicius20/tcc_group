a52d2ab3b4dd7a903b03a79a57bd1f9d46334d95
Lucas Vinicius <102090469+LucasVinicius20@users.noreply.github.com>
2025-08-06 14:10:13 -0300

    Add files via upload

--- TCC - PAGE/paginas/dashboard.php
+++ TCC - PAGE/paginas/dashboard.php
@@ -3,9 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dev Start Academy</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../styles/dashboard.css">
+    <title>Dev Start Academy - Dashboard</title>
 </head>
 <body>
-    <h1>Ola mundo!</h1>
+    
+    <nav id="sidebar">
+        <div id="user">
+            <img src="../images/avatar_zoro.jpg" id="user_avatar" alt="Avatar do Usuário" width="200px">
+
+            <p id="user_infos">
+                <span class="item-description">
+                    Roronoa Zoro
+                </span>
+                <span class="item-description">
+                    Espadachim Verde
+                </span>
+            </p>
+        </div>
+
+        <ul id="side_items">
+            <li class="side-items">
+                <a href="#">
+                    <i class="fa-solid fa-gear"></i>
+                    <span class="item-description">
+                        Configurações
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+<script src="../javascript/dashboard.js"></script>
 </body>
 </html>
