55328cc878e9d9714633506fc3d97aee4cbff432
Lucas Vinicius <102090469+LucasVinicius20@users.noreply.github.com>
2025-09-01 14:20:04 -0300

    Add files via upload
    com novas funcionalidades na tela de dashboar, novas imagens e botões clicaveis feito pelo Arthur

--- TCC - PAGE/paginas/dashboard.php
+++ TCC - PAGE/paginas/dashboard.php
@@ -6,27 +6,29 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../styles/dashboard.css">
     <title>Dev Start Academy - Dashboard</title>
+
+    
 </head>
 <body>
     
     <nav id="sidebar">
         <div id="sidebar_content">
-        <div id="user">
-            <img src="../images/avatar_zoro.jpg" id="user_avatar" alt="Avatar do Usuário">
+            <div id="user">
+                <img src="../images/avatar_zoro.jpg" id="user_avatar" alt="Avatar do Usuário">
 
-            <p id="user_infos">
-                <span class="item-description">
-                    Roronoa Zoro
-                </span>
-                <span class="item-description">
-                    Espadachim Verde
-                </span>
-            </p>
-        </div>
+                <p id="user_infos">
+                    <span class="item-description">
+                        Roronoa Zoro
+                    </span>
+                    <span class="item-description">
+                        Espadachim Verde
+                    </span>
+                </p>
+            </div>
 
-        <ul id="side_items">
+            <ul id="side_items">
 
-            <li class="side-item active">
+                <li class="side-item active">
                     <a href="#">
                         <i class="fa-solid fa-chart-line"></i>
                         <span class="item-description">
@@ -70,7 +72,7 @@
                         </span>
                     </a>
                 </li>
-        </ul>
+            </ul>
 
             <button id="open_btn">
                 <i id="open_btn_icon" class="fa-solid fa-chevron-right"></i>
@@ -86,7 +88,35 @@
     </nav>
 
     <main>
-        <h1>Seja bem-vindo! Ao seu site de estudos</h1>
+        <center><h1>Seja bem-vindo! Ao seu site de estudos</h1></center>
+
+        <section class="container">
+            <div class="card">
+                <a style="color: white; text-decoration: none;" href="http://localhost/TCC%20-%20PAGE/">
+                    <img src="../images/html_image_reference.png" alt="Curso HTML">
+                    <div class="card-content">
+                        <h2>Curso HTML - Básico</h2>
+                        <p>Neste curso, você aprenderá o básico de HTML.</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="small-cards">
+                <div class="small-card ">
+                    <a style="color: white;" href="http://localhost/TCC%20-%20PAGE/">
+                        <img class="image-size" src="../images/html_code_image.png" alt="Código HTML">
+                        <p><strong>Estrutura HTML Básica</strong></p>
+                    </a>
+                </div>
+
+                <div class="small-card">
+                    <a style="color: white;" href="http://localhost/TCC%20-%20PAGE/">
+                        <img src="../images/html_code_list_image.png" alt="Código HTML">
+                        <p><strong>Lista em HTML Básica</strong></p>
+                    </a>
+                </div>
+            </div>
+        </section>
     </main>
 
 <script src="../javascript/dashboard.js"></script>
