ba1071921541b3f60ca4e70cce232997469195f2
Lucas Vinicius <102090469+LucasVinicius20@users.noreply.github.com>
2025-11-27 15:00:38 -0300

    Add files via upload

--- TCC - PAGE/paginas/aulahtml.php
+++ TCC - PAGE/paginas/aulahtml.php
@@ -1,34 +1,38 @@
+<?php
+session_start();
+$current_page = basename($_SERVER['PHP_SELF']);
+include 'menu.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mural de Aulas HTML</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>Mural de Aulas</title>
   <link rel="stylesheet" href="../styles/aulahtml.css">
 </head>
 <body>
-  <header>
-    <h1 class="text-center font-size-header font-extra-bold">MURAL DE AULAS HTML</h1>
-  </header>
-
+  
   <main>
     <a href="http://localhost/TCC%20-%20PAGE/paginas/sos%20certo/index.php">
     <section id="aulas-section">
       <article class="aula">
         <div class="aula-imagem" style="background-image: url('../images/HTML.png');"></div>
-        <h2 class="titulo">Aula 1</h2>
+        <h2 class="titulo">Aula HTML</h2>
         <p class="aula-texto"></p>
       </article>
 
       <article class="aula">
-        <div class="aula-imagem" style="background-image: url('../images/HTML.png');"></div>
-        <h2 class="titulo">Aula 2</h2>
+        <div class="aula-imagem" style="background-image: url('../images/CSS.png');"></div>
+        <h2 class="titulo">Aula CSS</h2>
         <p class="aula-texto"></p>
       </article>
 
       <article class="aula">
-        <div class="aula-imagem" style="background-image: url('../images/HTML.png');"></div>
-        <h2 class="titulo">Aula 3</h2>
+        <div class="aula-imagem" style="background-image: url('../images/');"></div>
+        <h2 class="titulo">Aula HTML e CSS</h2>
         <p class="aula-texto"></p>
       </article>
     </section>
